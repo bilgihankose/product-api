dd778d583cc71c51e27f15fc4760abeebdbff967
bilgihankose <bilgihankose@gmail.cim>
2021-06-15 14:02:52 +0300

    added handlers

---
+++ handlers/productHandlers.go
@@ -0,0 +1,28 @@
+package handlers
+
+import "net/http"
+
+//HTTP POST - api/products
+func postProductHandler(w http.ResponseWriter, r *http.Request){
+
+}
+
+//HTTP GET - api/products
+func getProductHandler(w http.ResponseWriter, r *http.Request){
+
+}
+
+//HTTP GET -api/products/{id}
+func getProductIDHandler (w http.ResponseWriter, r *http.Request){
+
+}
+
+//HTTP PUT - api/products/{id]
+func putProductHandler(w http.ResponseWriter, r *http.Request){
+
+}
+
+//HTTP DELETE - api/products/{id}
+func deleteProductHandler(w http.ResponseWriter, r *http.Request)  {
+
+}
